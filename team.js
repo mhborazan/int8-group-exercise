b128e1c54f9a8077673ba34e4dc7186d01aa3d76
Thalia <thaliatelopoulou@gmail.com>
2023-04-06 16:46:17 +0300

    corrected mistakes

--- team.js
+++ team.js
@@ -1,13 +1,13 @@
 
 let theTeam = [{
-    name : thalia,
-    surname : telopoulou,
+    name : "thalia",
+    surname : "telopoulou",
     age : 25,
-    city : volos,
-    hobby : swimming,
-    favoriteFood : pizza,
-    favoriteVideoGame : theWitcher,
-    favoriteFilm : interstellar,
-    favoriteBook : theGreatGatsby,
-    petName : diego}
+    city : "volos",
+    hobby : "swimming",
+    favoriteFood : "pizza",
+    favoriteVideoGame : "theWitcher",
+    favoriteFilm : "interstellar",
+    favoriteBook : "theGreatGatsby",
+    petName : "diego"}
 ];
