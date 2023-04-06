61e94f853a826cca45e815c66616aa1bf9e7741e
Thalia <thaliatelopoulou@gmail.com>
2023-04-06 16:43:30 +0300

    added my array

--- team.js
+++ team.js
@@ -1,2 +1,13 @@
 
-let theTeam = [];
+let theTeam = [{
+    name : thalia,
+    surname : telopoulou,
+    age : 25,
+    city : volos,
+    hobby : swimming,
+    favoriteFood : pizza,
+    favoriteVideoGame : theWitcher,
+    favoriteFilm : interstellar,
+    favoriteBook : theGreatGatsby,
+    petName : diego}
+];
