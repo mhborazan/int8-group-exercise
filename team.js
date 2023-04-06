ea56147a3678cfa40b52cc1875320af4ff0dd3e4
Mustafa Hakan Borazan <borazanmh@outlook.com>
2023-04-06 16:47:57 +0300

    update

--- team.js
+++ team.js
@@ -1,2 +1,14 @@
 
-let theTeam = [];
+let theTeam = [obj];
+
+let obj = {
+    "name": "Hakan",
+    "surname": "Borazan",
+    "age": "29",
+    "hobby": "game dev",
+    "favoriteFood": "sarma",
+    "favoriteFilm": "Godfather",
+    "favoriteBook": "Robinson Cruose",
+    "petName": "none"
+
+}
