082d1513848bee6189565db73b4cd81546ec4682
Mustafa Hakan Borazan <borazanmh@outlook.com>
2023-04-06 16:50:32 +0300

    beautify

--- team.js
+++ team.js
@@ -1,13 +1,15 @@
 
-let theTeam = [{
-    "name": "Hakan",
-    "surname": "Borazan",
-    "age": "29",
-    "city": "Hatay",
-    "hobby": "game dev",
-    "favoriteFood": "sarma",
-    "favoriteFilm": "Godfather",
-    "favoriteBook": "Robinson Cruose",
-    "petName": "none"
-}];
+let theTeam = [
+    {
+        "name": "Hakan",
+        "surname": "Borazan",
+        "age": "29",
+        "city": "Hatay",
+        "hobby": "game dev",
+        "favoriteFood": "sarma",
+        "favoriteFilm": "Godfather",
+        "favoriteBook": "Robinson Cruose",
+        "petName": "none"
+    }
+];
 
