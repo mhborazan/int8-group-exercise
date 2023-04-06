c9c00ab7c056f6e6edae0c2d719076481f6d7102
Mustafa Hakan Borazan <borazanmh@outlook.com>
2023-04-06 16:49:52 +0300

    city added

--- team.js
+++ team.js
@@ -3,6 +3,7 @@ let theTeam = [{
     "name": "Hakan",
     "surname": "Borazan",
     "age": "29",
+    "city": "Hatay",
     "hobby": "game dev",
     "favoriteFood": "sarma",
     "favoriteFilm": "Godfather",
