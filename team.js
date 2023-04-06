a940d3d724910e4f58b09d99c352be9a0ad24e6e
Alptekin <alptekin.ocakdan@gmail.com>
2023-04-06 16:42:56 +0300

    initial commit

--- team.js
+++ team.js
@@ -1,2 +1,13 @@
 
-let theTeam = [];
+let theTeam = [
+    name="Alptekin",
+    surname="OCAKDAN",
+    age=23,
+    city="İstanbul",
+    hobby="gaming",
+    favoriteFood="French Fries",
+    favoriteVideoGame="Crysis",
+    favoriteFilm="Fast & Furious",
+    favoriteBook="Robin Hood",
+    petName="Aslan"
+];
