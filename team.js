802a563e67a2fe87bf9b251732c7b6ce8e4d1801
ahgsql <35695543+ahgsql@users.noreply.github.com>
2023-04-07 16:56:59 +0300

    Functions created together with Carlos, Hakan and Ali

--- team.js
+++ team.js
@@ -9,7 +9,7 @@ let theTeam = [
     favoriteVideoGame: "Rust",
     favoriteFilm: "The Shawshank Redemption",
     favoriteBook: "Holy Kuran",
-    petName: "Paşa",
+    petName: "",
   },
   {
     name: "Alptekin",
@@ -31,8 +31,9 @@ let theTeam = [
     hobby: "game dev",
     favoriteFood: "sarma",
     favoriteFilm: "Godfather",
+    favoriteVideoGame: "LOL",
     favoriteBook: "Robinson Cruose",
-    petName: "none",
+    petName: "",
   },
   {
     name: "thalia",
@@ -58,4 +59,99 @@ let theTeam = [
     favoriteBook: "the lean startup",
     petName: "Becky & Almos",
   },
+  {
+    name: "Ali Haydar",
+    surname: "Peter",
+    age: 19,
+    city: "Madrid",
+    hobby: "climbing",
+    favoriteFood: "pizza",
+    favoriteVideoGame: "God of War",
+    favoriteFilm: "Shrek",
+    favoriteBook: "the lean startup",
+    petName: "Becky & Almos",
+  },
+  {
+    name: "Alptekin",
+    surname: "OCAKDAN",
+    age: 23,
+    city: "İstanbul",
+    hobby: "gaming",
+    favoriteFood: "French Fries",
+    favoriteVideoGame: "Crysis",
+    favoriteFilm: "Fast & Furious",
+    favoriteBook: "Robin Hood",
+    petName: "Aslan",
+  },
+  {
+    name: "thalia",
+    surname: "telopoulou",
+    age: 25,
+    city: "volos",
+    hobby: "swimming",
+    favoriteFood: "pizza",
+    favoriteVideoGame: "theWitcher",
+    favoriteFilm: "interstellar",
+    favoriteBook: "theGreatGatsby",
+    petName: "diego",
+  },
 ];
+
+theTeam.sort((person1, person2) => {
+  if (person1.surname < person2.surname) {
+    return -1;
+  }
+});
+
+//console.log(theTeam);
+
+theTeam.sort((person1, person2) => {
+  if (person1.age < person2.age) {
+    return -1;
+  }
+});
+
+//console.log(theTeam);
+let sumOfAges = 0;
+theTeam.forEach((person) => {
+  sumOfAges += parseInt(person.age);
+});
+
+let avgAge = Math.floor(sumOfAges / theTeam.length);
+//console.log("The average age of team is ", avgAge);
+
+theTeam.forEach((person) => {
+  if (person.petName.length > 0) {
+    console.log(
+      `${person.name}  ${person.surname}  has pet, pet name is : ${person.petName}`
+    );
+  }
+});
+
+theTeam.forEach((person) => {
+  if (
+    person.favoriteVideoGame == "LOL" ||
+    person.favoriteVideoGame == "League Of Legends"
+  ) {
+    console.log(`${person.name}  ${person.surname} loves LOL`);
+  }
+});
+
+//Hakan's Solution
+for (let p = 0; p < theTeam.length; p++) {
+  for (let k = p + 1; k < theTeam.length; k++) {
+    if (theTeam[p].name == theTeam[k].name) {
+      console.log(theTeam[p].name);
+    }
+  }
+}
+
+//Ali's Solution
+theNamesArray = [];
+theTeam.forEach((person) => {
+  if (!theNamesArray.includes(person.name)) {
+    theNamesArray.push(person.name);
+  } else {
+    console.log(person.name);
+  }
+});
